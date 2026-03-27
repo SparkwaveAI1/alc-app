24ac04c61c64a98d3baf1a8cc5be6ec7d49b797f
Dev Agent <cumbucotrader@gmail.com>
2026-03-27 19:17:09 +0000

    fix: env placeholder fallbacks and AppShell refactor [SPA-2562]

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,18 +1,7 @@
-'use client'
+import { AppShell } from '@/components/layout/AppShell'
 
-import { Sidebar } from '@/components/layout/Sidebar'
-import { TopBar } from '@/components/layout/TopBar'
+export const dynamic = 'force-dynamic'
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-screen overflow-hidden">
-      <Sidebar />
-      <div className="flex-1 flex flex-col overflow-hidden">
-        <TopBar />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6">
-          {children}
-        </main>
-      </div>
-    </div>
-  )
+  return <AppShell>{children}</AppShell>
 }
